23bf083225e80b8aafdb60d6eb164b30f686b57a
OmkarD45 <omkardeshmukh162001@gmail.com>
2025-04-03 13:06:37 +0530

    03042025

--- script.js
+++ script.js
@@ -18,12 +18,34 @@ addTaskButton.addEventListener(("click"),()=>{
     }
     tasks.push(newTask);
     saveTasks();
+    renderTask(newTask)
     todoInput.value="";  //to clear input
     console.log(tasks)
 });
 
 function renderTask(task){
-    console.log(task.text);
+    const li=document.createElement('li');
+    li.setAttribute('data-id',task.id)
+    if(task.completed) li.classList.add('completed')
+    li.innerHTML=`
+    <span>${task.text}</span>
+    <button>Delete</button>
+    `;
+    li.addEventListener('click',(e)=>{
+        if(e.target.tagName==='BUTTON') return ;
+        task.completed=!task.completed;
+        li.classList.toggle('completed')
+        saveTasks();
+    });
+
+    li.querySelector('button').addEventListener('click',(e)=>{
+        e.stopPropagation();//prevent toggle from firing
+        tasks=tasks.filter((t)=>t.id===task.id)
+        li.remove();
+        saveTasks();
+    })
+    
+    todoList.appendChild(li);
 }
 
 function saveTasks(){
